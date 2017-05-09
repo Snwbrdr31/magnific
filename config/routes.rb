5eea62c1b3f82738cb328a245948bba8144dba5a
Snwbrdr31 <russian478@gmail.com>
2017-05-09 16:35:07 -0600

    fixed website was not running

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,13 @@
 Rails.application.routes.draw do
-  namespace :api do
-    get 'specsheets/index'
-  end
-
-  namespace :api do
-    get 'specsheets/show'
-  end
 
   # Root Route
   root 'home#index'
 
+  namespace :api do
+    get 'specsheets/index'
+  end
+
+  
   # React Router - NO ROUTES BELOW THIS!
   get '*unmatched_route', to: 'home#index'
 end
